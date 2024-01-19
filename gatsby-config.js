c5765a1f97a4ae1f01c4c7ba50da8b37b1161a96
Yash Kothekar <yashkothekar1900@gmail.com>
2024-01-19 13:11:50 -0500

    Initial set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,14 +11,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Yash Kothekar`,
+      summary: `who lives and studies at UMass Amherst building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    // social: {
+    //   twitter: `kylemathews`,
+    // },
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -118,7 +118,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/calendarView.png`, // This path is relative to the root of the site.
       },
     },
   ],
